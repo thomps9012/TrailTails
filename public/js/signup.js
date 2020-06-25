0fe2211488ad8f0804efafe6f08df17e997fd6d2
Samuel Thompson <thomps9012@gmail.com>
2020-06-25 16:42:24 -0400

    updated sign up sheet

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,36 +3,42 @@ $(document).ready(function() {
     var signUpForm = $("form.signup");
     var firstNameInput = $("input#firstName-input");
     var lastNameInput = $("input#lastName-input");
+    var homeTownInput = $("input#pac-input")
     var emailInput = $("input#email-input");
     var passwordInput = $("input#password-input");
-  
     // When the signup button is clicked, we validate the email and password are not blank
     signUpForm.on("submit", function(event) {
       event.preventDefault();
+      var homeTownText = homeTownInput.val().trim()
+      var homeTownArr = homeTownText.split(",")
+      console.log(homeTownArr)
       var userData = {
         firstName: firstNameInput.val().trim(),
         lastName: lastNameInput.val().trim(), 
+        homeCity: homeTownArr[0],
+        homeState: homeTownArr[1],
         email: emailInput.val().trim(),
         password: passwordInput.val().trim()
       };
-  
-      if (!userData.firstName || !userData.lastName || !userData.email || !userData.password) {
+      if (!userData.firstName || !userData.lastName || !userData.email || !userData.password || !userData.homeCity || !userData.homeState) {
         return;
       }
       // If we have an email and password, run the signUpUser function
-      signUpUser(userData.firstName, userData.lastName, userData.email, userData.password);
+      signUpUser(userData.firstName, userData.lastName, userData.homeCity, userData.homeState, userData.email, userData.password);
       firstNameInput.val("");
       lastNameInput.val("");
+      homeTownInput.val("");
       emailInput.val("");
       passwordInput.val("");
     });
-  
     // Does a post to the signup route. If successful, we are redirected to the members page
     // Otherwise we log any errors
-    function signUpUser(firstName, lastName, email, password) {
+    function signUpUser(firstName, lastName, homeCity, homeState, email, password) {
       $.post("/api/signup", {
         firstName: firstName,
         lastName: lastName,
+        homeCity: homeCity,
+        homeState: homeState,
         email: email,
         password: password
       })
@@ -42,10 +48,8 @@ $(document).ready(function() {
         })
         .catch(handleLoginErr);
     }
-  
     function handleLoginErr(err) {
       $("#alert .msg").text(err.responseJSON);
       $("#alert").fadeIn(500);
     }
   });
-  
