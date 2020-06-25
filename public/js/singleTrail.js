e2a56034abf71411f85b131c78bc85b392aecf40
Samuel Thompson <thomps9012@gmail.com>
2020-06-25 11:19:46 -0400

    added in carousel options

--- public/js/singleTrail.js
+++ public/js/singleTrail.js
@@ -5,42 +5,50 @@ const trailTails = require('./trailTails.js');
 trailTails.trailInfo();
 
 // google maps javascript
-function initMap() {
-        var directionsRenderer = new google.maps.DirectionsRenderer;
-        var directionsService = new google.maps.DirectionsService;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: {lat: 41.85, lng: -87.65}
-        });
-        directionsRenderer.setMap(map);
-        directionsRenderer.setPanel(document.getElementById('right-panel'));
+// REQUIRES jquery.livequery
+   $('.google-map iframe:visible').livequery(function() {
+     var mapFrame = $(this);
+     if (!$(mapFrame).hasClass('map-refreshed')) {
+       mapFrame.attr('src', mapFrame.attr('src')+'');
+       mapFrame.addClass('map-refreshed');
+    }
+// });
+// function initMap() {
+//         var directionsRenderer = new google.maps.DirectionsRenderer;
+//         var directionsService = new google.maps.DirectionsService;
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 7,
+//           center: {lat: 41.85, lng: -87.65}
+//         });
+//         directionsRenderer.setMap(map);
+//         directionsRenderer.setPanel(document.getElementById('right-panel'));
 
-        var control = document.getElementById('floating-panel');
-        control.style.display = 'block';
-        map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+//         var control = document.getElementById('floating-panel');
+//         control.style.display = 'block';
+//         map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
-        var onChangeHandler = function() {
-          calculateAndDisplayRoute(directionsService, directionsRenderer);
-        };
-        document.getElementById('start').addEventListener('change', onChangeHandler);
-        document.getElementById('end').addEventListener('change', onChangeHandler);
-      }
+//         var onChangeHandler = function() {
+//           calculateAndDisplayRoute(directionsService, directionsRenderer);
+//         };
+//         document.getElementById('start').addEventListener('change', onChangeHandler);
+//         document.getElementById('end').addEventListener('change', onChangeHandler);
+//       }
 
-      function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-        var start = document.getElementById('start').value;
-        var end = document.getElementById('end').value;
-        directionsService.route({
-          origin: start,
-          destination: end,
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-            directionsRenderer.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
+//       function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+//         var start = document.getElementById('start').value;
+//         var end = document.getElementById('end').value;
+//         directionsService.route({
+//           origin: start,
+//           destination: end,
+//           travelMode: 'DRIVING'
+//         }, function(response, status) {
+//           if (status === 'OK') {
+//             directionsRenderer.setDirections(response);
+//           } else {
+//             window.alert('Directions request failed due to ' + status);
+//           }
+//         });
+//       }
 
 //openweather map api and javascript
 (function() {
