4ff9455a4c6568784b8a5c197521b89d2623a582
Samuel Thompson <thomps9012@gmail.com>
2020-06-27 09:17:47 -0400

    added in imperial units for weather api call

--- public/js/singleTrail.js
+++ public/js/singleTrail.js
@@ -21,4 +21,37 @@ console.log(localStorage.getItem("trailId"))
     next.children(':first-child').clone().appendTo($(this));
   }
 });
+
+
+ //weather call data
+     function callWeather(traillat, traillong) {
+        var weatherAPIKey = "bdc52f64afd883566cab72d748eec127";
+        var forecastURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + traillat + "&lon=" + traillong + "&APPID=" + weatherAPIKey + "&units=imperial";
+
+        $.ajax({
+            url: forecastURL,
+            method: "GET",
+            dataType: "JSON",
+        }).then(function (data) {
+            $("#currentWeather").empty();
+            console.log(data);
+            //appends weather data to a carousel item
+            var title = $("<h5 class='card-title'>").text("Current Weather");
+            var card = $("<div>").addClass("card");
+            var description = $("<p>").addClass("card-text").text("Description: " + data.weather[0].description);
+            var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+            var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+            var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °K");
+            var mintemp = $("<p>").addClass("card-text").text("Min Temperature: " + data.main.temp_min + " °K");
+            var maxtemp = $("<p>").addClass("card-text").text("Max Temperature: " + data.main.temp_max + " °K");
+            var cardBody = $("<div>").addClass("card-body");
+
+
+            //merge and add to page
+            cardBody.append(title, description, temp, mintemp, maxtemp, humid, wind);
+            card.append(cardBody);
+            $("#currentWeather").append(card);
+
+        });
+    }
 });
