584166bc9623093af1c2a6a7ea0246ce615146d8
Andrew Murphy <amurphy37@gmail.com>
2020-06-30 16:01:41 -0400

    updated for heroku deploy

---
+++ config/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "development": {
+        "username": "root",
+        "password": "password",
+        "database": "TrailTails_db",
+        "host": "127.0.0.1",
+        "port": 3306,
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": "password",
+        "database": "TrailTails_db_test",
+        "host": "127.0.0.1",
+        "port": 3306,
+        "dialect": "mysql"
+    },
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql"
+    }
+}
